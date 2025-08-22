f6ba47ccd54c6c75ff80c95e130d3966c0915fe1
pablo dietrich <pablogustavodietrich@gmail.com>
2025-08-22 15:36:49 -0300

    subiendo 2 formularios vacios

---
+++ unidad 2 practica 7/aplicacion7a.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace unidad_2_practica_7
+{
+    public partial class aplicacion7a : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
